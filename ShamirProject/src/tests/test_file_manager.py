82a4519ba232da10b7ce7550bb3e1dae6fa6ef47
Gibran-RL <gibranramirez@ciencias.unam.mx>
2024-11-21 22:25:34 -0600

    file manager test: delete auxiliar functions test and fixes paths

--- ShamirProject/src/tests/test_file_manager.py
+++ ShamirProject/src/tests/test_file_manager.py
@@ -3,45 +3,11 @@ import os
 import sys
 sys.path.append(os.path.abspath("./src/main"))
 from file_manager import (
-    _validate_file,
-    _validate_directory,
     read_binary_file,
     write_binary_file,
     read_text_file,
     write_text_file
 )
-
-def test_validate_file_not_found():
-    """
-    Test that _validate_file raises FileNotFoundError
-    when the file does not exist.
-    """
-    with pytest.raises(FileNotFoundError):
-        _validate_file("resources/non_existent_file.txt")
-
-def test_validate_file_permission_error():
-    """
-    Test that _validate_file raises PermissionError
-    when the file exists but is not readable.
-    """
-    with pytest.raises(PermissionError):
-        _validate_file("resources/file_without_permission.txt")
-
-def test_validate_directory_not_found():
-    """
-    Test that _validate_directory raises FileNotFoundError
-    when the directory does not exist.
-    """
-    with pytest.raises(FileNotFoundError):
-        _validate_directory("resources/non_existent_directory/file.txt")
-
-def test_validate_directory_permission_error():
-    """
-    Test that _validate_directory raises PermissionError
-    when the directory exists but is not writable.
-    """
-    with pytest.raises(PermissionError):
-        _validate_directory("resources/directory_without_permission/file.txt")
 
 def test_read_binary_file_not_found():
     """
@@ -49,7 +15,7 @@ def test_read_binary_file_not_found():
     when the file does not exist.
     """
     with pytest.raises(FileNotFoundError):
-        read_binary_file("resources/non_existent_file.bin")
+        read_binary_file("src/tests/resources/non_existent_file.bin")
 
 def test_read_binary_file_permission_error():
     """
@@ -57,7 +23,7 @@ def test_read_binary_file_permission_error():
     when the file exists but is not readable.
     """
     with pytest.raises(PermissionError):
-        read_binary_file("resources/file_without_permission.bin")
+        read_binary_file("src/tests/resources/file_without_permission.bin")
 
 def test_write_binary_file_directory_not_found():
     """
@@ -65,7 +31,7 @@ def test_write_binary_file_directory_not_found():
     when the directory does not exist.
     """
     with pytest.raises(FileNotFoundError):
-        write_binary_file("resources/non_existent_directory/file.bin", b"data")
+        write_binary_file("src/tests/resources/non_existent_directory/file.bin", b"data")
 
 def test_write_binary_file_permission_error():
     """
@@ -73,7 +39,7 @@ def test_write_binary_file_permission_error():
     when the directory exists but is not writable.
     """
     with pytest.raises(PermissionError):
-        write_binary_file("resources/directory_without_permission/file.bin", b"data")
+        write_binary_file("src/tests/resources/directory_without_permission/file.bin", b"data")
 
 def test_read_text_file_not_found():
     """
@@ -81,7 +47,7 @@ def test_read_text_file_not_found():
     when the file does not exist.
     """
     with pytest.raises(FileNotFoundError):
-        read_text_file("resources/non_existent_file.txt")
+        read_text_file("src/tests/resources/non_existent_file.txt")
 
 def test_read_text_file_permission_error():
     """
@@ -89,7 +55,7 @@ def test_read_text_file_permission_error():
     when the file exists but is not readable.
     """
     with pytest.raises(PermissionError):
-        read_text_file("resources/file_without_permission.txt")
+        read_text_file("src/tests/resources/file_without_permission.txt")
 
 def test_write_text_file_directory_not_found():
     """
@@ -97,7 +63,7 @@ def test_write_text_file_directory_not_found():
     when the directory does not exist.
     """
     with pytest.raises(FileNotFoundError):
-        write_text_file("resources/non_existent_directory/file.txt", "data")
+        write_text_file("src/tests/resources/non_existent_directory/file.txt", "data")
 
 def test_write_text_file_permission_error():
     """
@@ -105,4 +71,4 @@ def test_write_text_file_permission_error():
     when the directory exists but is not writable.
     """
     with pytest.raises(PermissionError):
-        write_text_file("resources/directory_without_permission/file.txt", "data")
+        write_text_file("src/tests/resources/directory_without_permission/file.txt", "data")
