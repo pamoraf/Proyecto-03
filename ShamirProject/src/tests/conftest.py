0e7b86004dabaf0c60b37e43ad1c228f82875d00
Pablo <pablomorafo@ciencias.unam.mx>
2024-11-21 21:31:17 -0600

    Se arreglo error en la configuracion de directorios de los tests

--- ShamirProject/src/tests/conftest.py
+++ ShamirProject/src/tests/conftest.py
@@ -1,27 +1,28 @@
 import pytest
 import os
-import sys
-sys.path.append(os.path.abspath("./src/tests"))
+
+RESOURCES_DIR = "./src/tests/resources"
 
 @pytest.fixture(scope="module", autouse=True)
 def setup_and_teardown():
     original_permissions = {
-        "resources/file_without_permission.bin": os.stat("resources/file_without_permission.bin").st_mode,
-        "resources/file_without_permission.txt": os.stat("resources/file_without_permission.txt").st_mode,
-        "resources/directory_without_permission": os.stat("resources/directory_without_permission").st_mode,
+        os.path.join(RESOURCES_DIR, "file_without_permission.bin"): os.stat(os.path.join(RESOURCES_DIR, "file_without_permission.bin")).st_mode,
+        os.path.join(RESOURCES_DIR, "file_without_permission.txt"): os.stat(os.path.join(RESOURCES_DIR, "file_without_permission.txt")).st_mode,
+        os.path.join(RESOURCES_DIR, "directory_without_permission"): os.stat(os.path.join(RESOURCES_DIR, "directory_without_permission")).st_mode,
     }
-    os.chmod("resources/file_without_permission.bin", 0o000)
-    os.chmod("resources/file_without_permission.txt", 0o000)
-    os.chmod("resources/directory_without_permission", 0o555)
+    os.chmod(os.path.join(RESOURCES_DIR, "file_without_permission.bin"), 0o000)
+    os.chmod(os.path.join(RESOURCES_DIR, "file_without_permission.txt"), 0o000)
+    os.chmod(os.path.join(RESOURCES_DIR, "directory_without_permission"), 0o555)
     yield
-    os.chmod("resources/file_without_permission.bin", original_permissions["resources/file_without_permission.bin"])
-    os.chmod("resources/file_without_permission.txt", original_permissions["resources/file_without_permission.txt"])
-    os.chmod("resources/directory_without_permission", original_permissions["resources/directory_without_permission"])
+    os.chmod(os.path.join(RESOURCES_DIR, "file_without_permission.bin"), original_permissions[os.path.join(RESOURCES_DIR, "file_without_permission.bin")])
+    os.chmod(os.path.join(RESOURCES_DIR, "file_without_permission.txt"), original_permissions[os.path.join(RESOURCES_DIR, "file_without_permission.txt")])
+    os.chmod(os.path.join(RESOURCES_DIR, "directory_without_permission"), original_permissions[os.path.join(RESOURCES_DIR, "directory_without_permission")])
 
 @pytest.fixture(scope="session")
 def text_samples():
     text = []
-    path = "./resources/text"
+    path = os.path.abspath(os.path.join(RESOURCES_DIR, "text"))
+    print(path)
     for filename in os.listdir(path):
         with open(os.path.join(path, filename), 'r', encoding='utf-8') as file:
             text.append(file.read()) 
